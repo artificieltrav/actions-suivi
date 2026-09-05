d73de49e35cebdfbcd98cc88ef97101595a0bb66
artificieltrav <artificieltravail@gmail.com>
2026-09-05 15:00:45 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "actions-suivi-v4";
+const ASSETS = [
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+  // Ne jamais mettre en cache les liens externes (Google, Yahoo, proxys, etc.)
+  if (url.origin !== self.location.origin) return;
+
+  // Réseau en priorité : garantit qu'une mise à jour déployée est toujours vue.
+  // Le cache ne sert que de secours si l'appareil est hors-ligne.
+  event.respondWith(
+    fetch(event.request, { cache: "no-store" })
+      .then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
